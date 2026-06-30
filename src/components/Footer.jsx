2a6d9ebdeb4be54c3e3630f075dfb04bac67f1ae
mrunalimohite <mrunalimohite24@gmail.com>
2026-06-30 22:49:59 +0530

    Added resume PDF

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import './Footer.css';
 import { FaArrowCircleUp } from "react-icons/fa";
-
+import resume from "../assets/MrunaliMohiteResume .pdf"
 function Footer() {
   return (
         <footer className="footer">
             <p>© {new Date().getFullYear()}{" "}Mrunali Mohite </p>
-            <a href="/Mrunali-Mohite-Resume.pdf" download className="btn btn-primary" > Download Resume </a>
+            <a  href={resume} target="_blank" rel="noopener noreferrer"> Download Resume </a>
             <a href="#home" className="back-to-top" aria-label="Back to top"><FaArrowCircleUp /></a>
         </footer>
     );
